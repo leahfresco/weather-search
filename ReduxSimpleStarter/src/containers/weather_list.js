4ac8323bde91aa8e0640cd97b94da42859aceb03
Leah Yukelson <leahyukelson@gmail.com>
2017-08-10 19:26:04 -0700

    Change city name to Google map

--- ReduxSimpleStarter/src/containers/weather_list.js
+++ ReduxSimpleStarter/src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Chart from '../Components/chart';
+import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,10 +9,11 @@ class WeatherList extends Component {
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humids = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lat={lat} lon={lon} /></td>
         <td><Chart data={temps} color="green" units="K"/></td>
         <td><Chart data={pressures} color="blue" units="hPA"/></td>
         <td><Chart data={humids} color="red" units="%"/></td>
